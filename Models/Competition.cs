31b7e06c7b15c8f4ac44ee7ee40cb473cbb46e0d
Kirill Lifarenko <kirill.lifarenko@redexpress.ru>
2019-07-29 18:48:11 +0300

    adad

--- Models/Competition.cs
+++ Models/Competition.cs
@@ -21,11 +21,17 @@ namespace Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get { return _id; } set { Notify(ref _id, value, "Id"); } }
+
+        [Required]
         public string City { get { return _city; } set { Notify(ref _city, value, "City"); } }
+        [Required]
         public DateTime Date { get { return _date; } set { Notify(ref _date, value, "Date"); } }
+
         public bool IsClosed { get { return _isClosed; } set { Notify(ref _isClosed, value, "IsClosed"); } }
 
+        [Required]
         public int DiplomaNumber { get { return _diplomaNumber; } set { Notify(ref _diplomaNumber, value, "DiplomaNumber"); } }
+        [Required]
         public int DiplomaNumberDigits { get { return _diplomaNumberDigits; } set { Notify(ref _diplomaNumberDigits, value, "DiplomaNumberDigits"); } }
 
         [NotMapped]
