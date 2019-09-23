7f8732b42a6b1cbb96b8cc73c94d9ece692896aa
Kirill Lifarenko <kirill.lifarenko@redexpress.ru>
2019-09-23 18:41:41 +0300

    йв

---
+++ BestApp/ViewModels/Grades/GradesViewModel.cs
@@ -0,0 +1,39 @@
+﻿using BestApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BestApp.ViewModels.Grades
+{
+    public class GradesViewModel : ViewModelBase
+    {
+
+        public GradesViewModel()
+        {
+            this.context = new BestDbContext();
+
+            this.context.Grades.Load();
+        }
+
+        private readonly BestDbContext context;
+        private ObservableCollection<Grade> grades;
+        
+        public ICommand NewGradeCommand { get; private set; }
+        public ICommand OpenGradeCommand { get; private set; }
+        public ICommand RefreshCommand { get; private set; }
+
+        public ObservableCollection<Grade> Grades
+        {
+            get => grades;
+            set => Notify(ref grades, value);
+        }
+
+
+    }
+}
